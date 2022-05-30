da7cdef66453c30b0e891004fa232c0adead4c87
Nadira Ali <nadiraali188a@gmail.com>
2022-05-30 13:05:54 +0100

    defined necessary query and mutation types

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,52 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+    users: [User]
+  }
+
+  input SaveBookInput {
+    authors: [String]
+    title: String!
+    bookId: String!
+    description: String!
+    image: String
+    link: String
+  }
+
+  input RemoveBookInput {
+    bookId: String!
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(input: SaveBookInput!): User!
+    removeBook(input: RemoveBookInput!): User!
+  }
+`;
+
+module.exports = typeDefs;
